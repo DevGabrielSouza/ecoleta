809a8f309a7af2eb31a490e743b9316ed993cbf9
Gabriel Souza <contato@devgabrielsouza.com.br>
2020-11-19 03:37:32 -0300

    Create simple test routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,15 +2,40 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('Usuários');
+// Rota: Endereço completo da requisição
+// Entidade: Entidade acessada
 
-    response.json([
-        'Diego',
-        'Fernando',
-        'Júlia',
-        'Larissa'
-    ]);
+const users = [
+    'Diego',
+    'Fernando',
+    'Júlia',
+    'Larissa'
+];
+
+
+app.get('/users', (request, response) => {
+    
+    const search = String(request.query.search);
+
+    // Se não existir o param search, será atribuido a variável users
+    const filteredUser = request.query.search ? users.filter( user => user.includes(search) ) : users;
+
+    return response.json(filteredUser);
+});
+
+app.get('/users/:id', (request, response) => {
+
+    const userId = Number(request.params.id);
+
+    const user = users[userId];
+
+    if (user){
+        return response.json(user)
+    }else{
+        return response.json({'msg' : 'Usuário não existe'});
+    }
+
+    
 });
 
 app.listen(3333);

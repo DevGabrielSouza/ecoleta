54f0dc66e93041633fa320c124b3aa9295f6f37a
Gabriel Souza <contato@devgabrielsouza.com.br>
2020-11-28 14:18:05 -0300

    create database sqlite3 and migrations

---
+++ server/src/routes.ts
@@ -0,0 +1,10 @@
+
+import express, { RouterOptions } from 'express';
+
+const routes = express.Router();
+
+routes.get('/', (request, response) => {
+    //
+});
+
+export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,41 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-// Rota: Endereço completo da requisição
-// Entidade: Entidade acessada
+app.use(express.json());
 
-const users = [
-    'Diego',
-    'Fernando',
-    'Júlia',
-    'Larissa'
-];
-
-
-app.get('/users', (request, response) => {
-    
-    const search = String(request.query.search);
-
-    // Se não existir o param search, será atribuido a variável users
-    const filteredUser = request.query.search ? users.filter( user => user.includes(search) ) : users;
-
-    return response.json(filteredUser);
-});
-
-app.get('/users/:id', (request, response) => {
-
-    const userId = Number(request.params.id);
-
-    const user = users[userId];
-
-    if (user){
-        return response.json(user)
-    }else{
-        return response.json({'msg' : 'Usuário não existe'});
-    }
-
-    
-});
 
 app.listen(3333);

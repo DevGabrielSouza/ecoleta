6367f09af0a7429d8861a252f5f58bc7519fc8ac
Gabriel Souza <contato@devgabrielsouza.com.br>
2020-12-13 18:23:37 -0300

    Create header component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import './App.css';
 
+// Components
+
+import Header from './Components/Header';
+
 function App() {
   return (
     <div className="App">
+
+      <Header/>
+
       <h1>Hello World</h1>
     </div>
   );

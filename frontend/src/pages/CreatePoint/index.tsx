96b694de707024e32cab3678a03f4fc5e5726116
Gabriel Souza <contato@devgabrielsouza.com.br>
2020-12-26 18:02:12 -0300

    Add handle actions

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useLayoutEffect, ChangeEvent } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import './style.css';
 
 import logo from '../../assets/logo.svg';
 import api from '../../services/api';
+import axios from 'axios';
 
 interface Item{
     id: number,
@@ -18,10 +19,18 @@ interface Estado{
     nome: string,
 }
 
+interface City{
+    id: number,
+    nome: string,
+}
+
 const CreatePoint = () => {
 
     const [items, setItems] = useState<Array<Item>>([]);
     const [estados, setEstados] = useState<Array<Estado>>([]);
+    const [cities, setCities] = useState<Array<City>>([]);
+    const [selectedUf, setSelectedUf] = useState('0');
+    const [selectedCity, setSelectedCity] = useState('0');
 
     useEffect(() => {
 
@@ -29,13 +38,38 @@ const CreatePoint = () => {
             setItems(response.data);
         })
 
-        fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome').then( async responseEstados => {
-            let estados = await responseEstados.json()
-            setEstados(estados);
-        })
-
     }, []);
 
+    useEffect( () => {
+
+        axios.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome').then( response => {
+            let estados = response.data;
+            setEstados(estados);
+        });
+    }, []);
+
+    useEffect( () => {
+
+        axios.get(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`).then( response => {
+            let cities = response.data;
+            setCities(cities);
+        });
+
+    }, [selectedUf]);
+
+    function handleSelectedUf(event: ChangeEvent<HTMLSelectElement>){
+        let selectedUf = event.target.value;
+        setSelectedUf(selectedUf);
+    }
+
+    function handleSelectedCity(event: ChangeEvent<HTMLSelectElement>){
+        let selectedCity = event.target.value;
+        setSelectedCity(selectedCity);
+    }
+
+    function handleMapClick(){
+        //
+    }
     
 
 
@@ -114,12 +148,12 @@ const CreatePoint = () => {
                         <div className="field-group">
 
                             <div className="field">
-                                <label htmlFor="uf">Estado (UF)</label>
-                                <select name="uf" id="uf">
+                                <label htmlFor="uf">Estado (UF) (selected: {selectedUf})</label>
+                                <select name="uf" id="uf" onChange={handleSelectedUf} value={selectedUf}>
                                     <option value="0">Selecione uma UF</option>
 
                                     {estados.map(estado => (
-                                        <option key={estado.id} value={estado.id}>{estado.sigla}</option>
+                                        <option key={estado.id} value={estado.sigla}>{estado.sigla}</option>
                                     ))}
 
                                 </select>
@@ -127,8 +161,13 @@ const CreatePoint = () => {
 
                             <div className="field">
                                 <label htmlFor="city">Cidade</label>
-                                <select name="city" id="city">
+                                <select name="city" id="city" onChange={handleSelectedCity}>
                                     <option value="0">Selecione uma cidade</option>
+
+                                    {cities.map(city => (
+                                        <option key={city.id} value={city.nome}>{city.nome}</option>
+                                    ))}
+
                                 </select>
                             </div>
                             
